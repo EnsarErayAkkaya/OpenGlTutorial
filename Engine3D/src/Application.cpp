f8b11d2a25ede408e4e78878e07d767cc058f2d7
Ensar Eray Akkaya <33553090+EnsarErayAkkaya@users.noreply.github.com>
2023-03-19 00:20:47 +0300

    Projection, View and model matrixes added and pixel coordinates system added

--- Engine3D/src/Application.cpp
+++ Engine3D/src/Application.cpp
@@ -30,7 +30,7 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -50,10 +50,10 @@ int main(void)
 
     {
         float positions[]  {
-            -0.5f, -0.5f, 0.0f, 0.0f, // 0
-             0.5f, -0.5f, 1.0f, 0.0f, // 1
-             0.5f,  0.5f, 1.0f, 1.0f, // 2
-            -0.5f,  0.5f, 0.0f, 1.0f, // 3
+            100.0f, 100.0f, 0.0f, 0.0f, // 0
+            200.0f, 100.0f, 1.0f, 0.0f, // 1
+            200.5f, 200.0f, 1.0f, 1.0f, // 2
+            100.5f, 200.0f, 0.0f, 1.0f, // 3
         };
 
         unsigned int indicies[] = {
@@ -78,12 +78,16 @@ int main(void)
 
         IndexBuffer ib(indicies, 9);
 
-        glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(500, 0, 0));
+
+        glm::mat4 mvp = proj * view * model;
 
         Shader shader("res/shaders/Basic.Shader");
         shader.Bind();
         //shader.SetUniform4f("u_Color", .5f, .3f, .8f, 1.0f);
-        shader.SetUniformMatrix4f("u_MVP", proj);
+        shader.SetUniformMatrix4f("u_MVP", mvp);
 
         Texture texture("res/textures/test.png");
         texture.Bind();
